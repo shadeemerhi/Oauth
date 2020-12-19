63b41d7f00beb4de7421ba4a4bdca27e8030d5bb
Shadee Merhi <shadmerhi@gmail.com>
2020-12-19 00:27:31 -0700

    updated README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+const axios = require('axios');
 
-function App() {
+export default function App() {
+  
+  useEffect(() => {
+    axios.get('/api/data').then(response => {
+      console.log(response.data);
+    })
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      This is the app
+    </h1>
   );
 }
 
-export default App;
